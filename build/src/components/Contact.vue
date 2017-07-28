5054106493d6680ff7031db4b8f9698fe8531807
Andrew Bonifacio <hello@andrewbdesign.com>
2017-07-28 21:21:14 +1000

    Added contact form

--- build/src/components/Contact.vue
+++ build/src/components/Contact.vue
@@ -1,9 +1,53 @@
 <template>
     <div>
-      <h1>{{msg}}</h1>
-        This is the CONTACT section
+        <div id="hero-section" class="contact-hero">
+
+			<div class="icon-letter-container">
+				<img class='letter-c-icon' src="../assets/img/letters/letter-c-icon.png" alt="">
+				<img class="letter-c-bg" src="../assets/img/letters/letter-c-bg.jpg" alt="">
+			</div>
+			<div class="email-container">
+				<div class="container">
+					<div class="row">
+						<div class="tweleve columns">
+							<a href="mailto:contact@redscope.com">
+								<h1>Contact@redscope.com</h1>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="six columns icon-column">
+							<p><img class="icons icon-pin" src="../assets/img/icon/icon-pin.svg" alt="">319-325 Trafalgar St,<br>Petersham NSW 2049</p>
+						</div>
+						<div class="six columns icon-column">
+							<p><img class="icons icon-phone" src="../assets/img/icon/icon-phone.svg" alt=""><a href="tel:+61406685233">0406 685 233</a></p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="twelve columns">
+							<div class="social-media-menu">
+							<a href="https://www.facebook.com/redscopefilms/">
+								<img src="../assets/img/icon/logo-facebook.svg" alt="">
+							</a>
+							<a href="https://www.instagram.com/redscopefilms/">
+								<img src="../assets/img/icon/logo-instagram.svg" alt="">
+							</a>
+							<a href="https://vimeo.com/redscopefilms">
+								<img src="../assets/img/icon/logo-vimeo.svg" alt="">
+							</a>
+						</div>
+						</div>
+					</div>
+
+				</div>
+
+
+			</div>
+
+		</div>
         <app-contactform></app-contactform>
-        <!-- <app-instagramfeed></app-instagramfeed> -->
     </div>
 </template>
 
--- build/src/components/contact-form/ContactForm.vue
+++ build/src/components/contact-form/ContactForm.vue
@@ -1,13 +1,218 @@
 <template lang="html">
-  <div class="">
-    contact form fammmm
-  </div>
+    <div id="email-form-section">
+  			<div class="container">
+  				<div class="email-container-template">
+  					<form action="https://formspree.io/your@email.com" method="POST">
+  						Name:
+  						<input type="text" name="name">
+  						Email:
+  						<input type="email" name="email">
+  						Contact Number:
+  						<input type="text" name="contact_no">
+  						Subject:
+  						<input type="text" name="subject">
+  						Message:
+  						<textarea name="message"></textarea>
+  						<input class="submit-btn" type="submit" value="Send">
+  					</form>
+  				</div>
+  			</div>
+
+  		</div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
-<style lang="css">
+<style lang="scss">
+$mobile: 560px;
+$tablet: 820px;
+$desktop: 1024px;
+#hero-section {
+    .icon-letter-container {
+        position: absolute;
+
+        width: 320px;
+        height: 450px;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+
+        top: -100px;
+        left: -40%;
+
+    }
+    .letter-c-icon {
+
+        position: relative;
+        top: 0;
+        left: 0;
+        z-index: 20;
+        width: 160px;
+        height: 200px;
+    }
+    .letter-c-bg {
+        position: absolute;
+        left: 0;
+
+        width: 154px;
+        z-index: 19;
+        height: 163px;
+    }
+    @media (min-width: $tablet) {
+
+        .icon-letter-container {
+            top: 50px;
+            left: -50%;
+            .letter-c-bg {
+                width: 300px;
+                z-index: 19;
+                height: 298px;
+            }
+            .letter-c-icon {
+                width: 270px;
+                height: 340px;
+            }
+        }
+    }
+}
+
+.contact-hero {
+    background: #151515 !important;
+    a {
+        text-decoration: none;
+        color: #fff;
+        text-align: center;
+    }
+    p {
+        text-align: center;
+    }
+    h1 {
+        color: #606060;
+        text-align: center;
+        text-decoration: none;
+        font-size: 30px;
+        font-family: 'Fira Sans', sans-serif;
+        position: relative;
+        left: -20px;
+        margin-bottom: 20px;
+    }
+    @media (min-width: $mobile) {
+        h1 {
+            font-size: 40px;
+        }
+        .icon-column {
+            text-align: center;
+            a {
+                text-align: center;
+            }
+        }
+    }
+    @media (min-width: $tablet) {
+        h1 {
+            font-size: 50px;
+        }
+        .icon-column {
+            text-align: left;
+        }
+    }
+    .icons {
+        width: 20px;
+        height: 20px;
+        position: relative;
+        margin-left: -20px;
+
+    }
+    .icon-pin {
+        top: 4px;
+        right: 6px;
+    }
+    .icon-email {
+        right: 10px;
+        top: 5px;
+    }
+    .icon-phone {
+        right: 10px;
+        top: 5px;
+    }
+    .email-container {
+        padding: 230px 0;
+        max-width: 780px;
+        margin: 0 auto;
+
+        color: #fff;
+        position: relative;
+        z-index: 30;
+        .container {
+            max-width: 520px;
+        }
+        .social-media-menu {
+            text-align: center;
+
+            a img {
+                width: 40px;
+                display: inline-block;
+                margin: 20px 10px 0;
+            }
+        }
+    }
+    @media (min-width: $tablet) {
+        .email-container {
+            padding-left: 130px;
+            .container {
+                max-width: 520px;
+                margin-right: 0;
+            }
+        }
+    }
+
+}
+#email-form-section {
+    padding: 100px 0;
+    background: #f2f2f2;
+
+    .container {
+        max-width: 590px;
+    }
+    .email-container-template {
+        background: #FFF;
+        z-index: 200;
+        position: relative;
+        padding: 30px;
+        font-family: 'Fira Sans', sans-serif;
+        -webkit-box-shadow: 0 5px 17px 2px rgba(0,0,0,0.23);
+        -moz-box-shadow: 0 5px 17px 2px rgba(0,0,0,0.23);
+        box-shadow: 0 5px 17px 2px rgba(0,0,0,0.23);
+
+        input {
+            border-radius: 0;
+            display: block;
+            width: 100%;
+            margin-bottom: 10px;
+            background: #e5e5e5;
+            border: none;
+        }
+        textarea {
+            border-radius: 0;
+            display: block;
+            width: 100%;
+            margin-bottom: 10px;
+            height: 100px;
+            margin-top: 0;
+            background: #e5e5e5;
+            border: none;
+        }
+
+        .submit-btn {
+            background: #c1161c;
+            width: 200px;
+            margin-right: 0;
+            margin-left: auto;
+            position: relative;
+            color: #fff;
+        }
+    }
+
+}
 </style>
